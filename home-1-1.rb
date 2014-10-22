c6f6936d41ffcdc3bcaf5cb8524cafd83a156e54
Dawit <dawityhaile@gmail.com>
2014-10-21 18:01:15 -0700

    rewrot yield useage

--- home-1-1.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/ruby
-
-puts"IIIIIIII   LL      oooo  eeeeeee   u  u  "
-puts"  II       LL      o  o  e         u  u  "
-puts"  II       LL      o  o  eeeeee    u  u  "
-puts"  II       LL      o  o  e         u  u "
-puts"IIIIIII    LLLLLL  oooo  eeeeee    uuuu "
